8f545d2cb5c942d07f8b79b425999d9ea28a724a
test <juraev.abubakr_98@mail.ru>
2024-08-14 12:18:37 +0500

    done I think

--- app2.js
+++ app2.js
@@ -1,56 +1,36 @@
 const card = document.getElementById("card");
+const gloss = document.getElementById("gloss");
 
 card.addEventListener("mousemove", (e) => {
-  const pointerX = e.clientX; // x-coordinate relative to the left edge of the browser;
-  const pointerY = e.clientY; // y-coordinate relative to the top edge of the browser;
+  const pointerX = e.clientX; // x-coordinate of mouse cursor relative to the left edge of the browser;
+  const pointerY = e.clientY; // y-coordinate of mouse cursor relative to the top edge of the browser;
 
   const cardRect = card.getBoundingClientRect(); // it returns a DOMRect object representing the size of an element and its position relative to the viewport;
 
   const halfWidth = cardRect.width / 2;
   const halfHeight = cardRect.height / 2;
 
-  const cardCenterX = cardRect.left + halfWidth;
-  const cardCenterY = cardRect.top + halfHeight;
+  const cardCenterX = cardRect.left + halfWidth; // x-coordinate of the card center relative to the left edge;
+  const cardCenterY = cardRect.top + halfHeight; // y-coordinate of the card center relative to the left edge;
 
   const deltaX = pointerX - cardCenterX; // x value relative to the card center;
   const deltaY = pointerY - cardCenterY; // y value relative to the card center;
 
-  const distanceToCenter = Math.sqrt(deltaX * deltaX + deltaY * deltaY); // from the point to the center of the card;
+  const distanceToCenter = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)); // from the point to the center of the card;
 
   const maxDistance = Math.max(halfWidth, halfHeight); // the maximum distance from the corner to the center of the card;
 
-  // console.log("MAX Distance", maxDistance);
-  // console.log("distance to center", distanceToCenter/maxDistance);
-
-  const degree = mapNumberRange(distanceToCenter, 0, maxDistance, 0, 10); //?????????
-  const degree1 = (distanceToCenter * 10) / maxDistance;
-
-  // console.log(degree, degree1);
-
-  const rx = mapNumberRange(deltaY, 0, halfWidth, 0, 1); //?????????
-  const ry = mapNumberRange(deltaX, 0, halfHeight, 0, 1); //?????????
-
-  // console.log("rx", rx);
-  // console.log("ry", ry);
+  const degree = (distanceToCenter * 10) / maxDistance;
+  const rx = deltaY / halfHeight;
+  const ry = deltaX / halfWidth;
 
   card.style.transform = `perspective(400px) rotate3d(${-rx}, ${ry}, 0, ${degree}deg)`;
 
-  // gloss.style.transform = `translate(${-ry * 100}%, ${-rx * 100}%) scale(2.4)`;
-
-  // gloss.style.opacity = `${mapNumberRange(
-  //   distanceToCenter,
-  //   0,
-  //   maxDistance,
-  //   0,
-  //   0.6
-  // )}`;
+  gloss.style.transform = `translate(${-ry * 100}%, ${-rx * 100}%) scale(2.4)`;
+  gloss.style.opacity = (distanceToCenter * 0.6) / maxDistance;
 });
 
 card.addEventListener("mouseleave", () => {
   card.style = null;
-  // gloss.style.opacity = 0;
+  gloss.style.opacity = 0;
 });
-
-function mapNumberRange(n, a, b, c, d) {
-  return ((n - a) * (d - c)) / (b - a) + c;
-}
